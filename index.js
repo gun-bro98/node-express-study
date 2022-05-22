dd79da3178fbb010e51e2d8ea7e5f4b3b2cd59be
gunhyung <one0374@naver.com>
2022-05-22 17:03:08 +0900

    시퀄라이즈를 이용해서 mysql과 연결 성공

--- index.js
+++ index.js
@@ -1,78 +1,47 @@
 const express = require('express');
-const bird = require('./routes/bird')
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const {sequelize} = require('./models');
+
+// const bird = require('./routes/bird')
 const app = express();
-const port = 3000;
+app.set('port', process.env.PORT || 3001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+sequelize.sync({force: false})
+  .then(() => {
+    console.log('데이터베이스 연결');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 
-app.use('/bird', bird)
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-// 사용자정의 미들웨어
-const myLogger = function (req, res, next) {
-  console.log('LOGGED');
-  next();
-};
-
-const requestTime = function (req, res, next) {
-  req.requestTime = Date.now();
-  next();
-};
-
-app.use(requestTime);
-app.use(myLogger);
-
-
-app.get('/', (req, res, next) => {
-  if(req.requestTime){
-    console.log(req.requestTime);
-  }
-  next();
-}, (req, res ) => {
-  res.send('hello world!');
-})
-
-app.get('/about', (req, res) => {
-  res.send('about!');
-})
-
-app.get('/random.text', (req, res) => {
-  res.send('random.text');
-})
-
-app.get('/g(un)?bro', (req, res) => {
-  res.send('gunbro!');
-})
-
-app.get(/gun/, (req, res) => {
-  res.send('뷁!');
-})
-
-const cb0 = function (req, res, next) {
-  console.log('CB0');
-  next();
-}
-
-const cb1 = function (req, res, next) {
-  console.log('CB1');
-  next();
-}
-
-app.get('/example/d', [cb0, cb1], function (req, res, next) {
-  console.log('the response will be sent by the next function ...');
-  next();
-}, function (req, res) {
-  res.send('Hello from D!');
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
-app.route('/book')
-  .get(function(req, res) {
-    res.send('Get a random book');
-  })
-  .post(function(req, res) {
-    res.send('Add a book');
-  })
-  .put(function(req, res) {
-    res.send('Update the book');
-  });
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err: {};
+  res.status(err.status || 500);
+  res.render('error');
 
-app.listen(port, () => {
-  console.log(`현재 서버는 ${port}에서 돌고 있습니다.`)
+})
+
+// app.use('/bird', bird)
+
+app.listen(app.get('port'), () => {
+  console.log(`현재 서버는 ${app.get('port')}에서 돌고 있습니다.`)
 })
---
+++ models/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+const db = {};
+
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
+db.sequelize = sequelize;
+
+module.exports = db;
